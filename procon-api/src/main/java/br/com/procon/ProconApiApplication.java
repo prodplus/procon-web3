a59854b2f521659cb462dd38669a7c9585e2885c
Marlon F Garcia <marlon_garcia@outlook.com>
2022-07-26 08:40:17 -0300

    primeiro commit

---
+++ procon-api/src/main/java/br/com/procon/ProconApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.procon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProconApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProconApiApplication.class, args);
+	}
+
+}
---
+++ procon-api/src/test/java/br/com/procon/ProconApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.procon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProconApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
